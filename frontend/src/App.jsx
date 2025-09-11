e9e6460043de87a1574f791e699adcb21b91a4c8
karthikkishorkk <karthikkishor.4c@gmail.com>
2025-09-11 14:24:10 +0530

    Citizen & Admin Dashboard UI Implemented

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,53 +1,35 @@
-<<<<<<< HEAD
 // App.jsx
-import { Routes, Route } from 'react-router-dom'
-import LoginPage from './pages/Auth/LoginPage'
-import CitizenHome from './pages/Citizen/CitizenHome'
-import AdminHome from './pages/Admin/AdminHome'
-import ViewComplaint from "./pages/Admin/ViewComplaint";
+import { Routes, Route } from "react-router-dom";
 
-function App() {
-  return (
-    <Routes>
-      <Route path="/" element={<LoginPage />} />
-      <Route path="/citizen/home" element={<CitizenHome fullName="Karthik" />} />
-      <Route path="/officer/home" element={<AdminHome />} />
-      <Route path="/officer/complaints/:id" element={<ViewComplaint />} /> 
-    </Routes>
-  )
-}
-
-export default App
-=======
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home/Home";
 import LoginPage from "./pages/Auth/LoginPage";
-import CitizenHome from "./pages/Citizen/CitizenHome"; 
-import PrivateRoute from "./pages/Auth/Privateroute";  
+import CitizenHome from "./pages/Citizen/CitizenHome";
+import AdminHome from "./pages/Admin/AdminHome";
+import ViewComplaint from "./pages/Admin/ViewComplaint";
+import PrivateRoute from "./pages/Auth/Privateroute";
 import ReportList from "./pages/Citizen/ReportList";
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        {/* Public route */}
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/citizenhome" element={<CitizenHome />} />
-        
-        <Route
-          path="/report"
-          element={
-            <PrivateRoute>
-              <ReportList />
-            </PrivateRoute>
-          }
-        />
+    <Routes>      
+      <Route path="/" element={<Home />} />
+      <Route path="/login" element={<LoginPage />} />
+      <Route path="/citizen/home" element={<CitizenHome fullName="Karthik" />} />
+      <Route path="/officer/home" element={<AdminHome />} />
+      <Route path="/officer/complaints/:id" element={<ViewComplaint />} />
+      <Route
+        path="/report"
+        element={
+          <PrivateRoute>
+            <ReportList />
+          </PrivateRoute>
+        }
+      />
 
-        {/* Default redirect */}
-        <Route path="*" element={<LoginPage />} />
-      </Routes>
-    </Router>
+      {/* Default redirect */}
+      <Route path="*" element={<LoginPage />} />
+    </Routes>
   );
 }
 
 export default App;
->>>>>>> c1234bc8626815cb4f1c0348925bb5be1f0f430a
--- frontend/src/pages/Admin/AdminHome.jsx
+++ frontend/src/pages/Admin/AdminHome.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
-export default function OfficerHome() {
+export default function AdminHome() {
   const navigate = useNavigate();
   const [complaints, setComplaints] = useState([
     {
